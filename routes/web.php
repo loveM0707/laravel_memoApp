0f8d3806abd5dddb21db12857ce9d4c10a9a33c2
loveM0707 <lovemone0707@gmail.com>
2023-10-29 14:26:38 +0900

    Changed top page routing

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -33,6 +29,7 @@ require __DIR__.'/auth.php';
 
 Auth::routes();
 
+Route::get('/', [HomeController::class, 'index'])->name('index');
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::post('/store', [HomeController::class, 'store'])->name('store');
 Route::get('/edit/{id}', [HomeController::class, 'edit'])->name('edit');
